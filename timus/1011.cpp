f744b5f87d6c4a29056a38c787fee07d3a589e15
nitinnarwal <narwalnitin3@gmail.com>
2022-01-09 01:47:57 +0530

    Temp code for non bits/stdc++

---
+++ timus/1011.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+#define int long long
+const int M = 1e9 + 7;
+const int INF = 1e18 + 5;
+
+void solve(){
+    cout << "hello\n";
+
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
