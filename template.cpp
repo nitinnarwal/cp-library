22d3812c6aef7e3b4936e9ee21345852c4f7235e
nitinnarwal <40078927+nitinnarwal@users.noreply.github.com>
2022-10-08 02:43:11 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,10 +4,19 @@ using namespace std;
 #define M 1000000007
 #define INF ((1LL<<62LL) - 1)
 
+void solve(){
+    
+    
+    
+}
+
 signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-
-
+    
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
 }
